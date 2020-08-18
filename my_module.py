40469ea1e4fd1ce781fe17ae8e55b58ae029487b
Hirai Fujii <h.fujii.sys20@morijyobi.ac.jp>
2020-08-19 00:39:44 +0900

    06-05 金魚の糞

--- my_module.py
+++ my_module.py
@@ -1,2 +1,10 @@
-import my_module
-print(my_module.func(5))
+def func(v):
+    i = v + 3
+    return i
+
+
+import sys
+print(sys.path)
+
+def func_in_module_under_dir():
+    print("This is '{}'.".format(__name__))
